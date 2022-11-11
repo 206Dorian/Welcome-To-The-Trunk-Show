ab50d83b186c5f95cf864121ec4612a096f519a4
Dorian Birch <206dorian@gmail.com>
2022-11-11 14:55:33 -0600

    adding everything

--- Unsolved/models/index.js
+++ Unsolved/models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-// Categories have many Products
+// Categories hasMany Products
 
 // Products belongToMany Tags (through ProductTag)
 
