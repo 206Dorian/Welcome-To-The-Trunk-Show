66017673b014541c7eefabeb320625801033393f
Dorian Birch <206dorian@gmail.com>
2022-11-16 17:11:19 -0600

    tryign to figure out routes and test in insomnia

--- Unsolved/routes/api/category-routes.js
+++ Unsolved/routes/api/category-routes.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll({include:[Product]});
+    const categoryData = await Category.findAll({ include: [Product] });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -14,13 +13,25 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({ include: [Product] });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const locationData = await Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
--- Unsolved/routes/api/tag-routes.js
+++ Unsolved/routes/api/tag-routes.js
@@ -3,17 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+  try {
+    const categoryData = await Category.findAll({ include: [Product] });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.findOne({ include: [Product] });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+  try {
+    const locationData = await Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
